81232f22a9ba48feafc26be1e26a0cc165a4acb6
jmenarin <jpmenarin@icloud.com>
2023-07-21 17:25:18 -0300

    Tela Login

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list_provider/app/core/database/sqlite_adm_connection.dart';
 import 'package:todo_list_provider/app/core/database/sqlite_connection_factory.dart';
+import 'package:todo_list_provider/app/core/ui/todo_list_ui_config.dart';
+import 'package:todo_list_provider/app/modules/auth/auth_module.dart';
 import 'package:todo_list_provider/app/modules/auth/login/login_controller.dart';
 import 'package:todo_list_provider/app/modules/auth/login/login_page.dart';
 import 'package:todo_list_provider/app/modules/splash/splash_page.dart';
@@ -32,13 +34,10 @@ class _AppWidgetState extends State<AppWidget> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Todo List Provider',
+      initialRoute: '/login',
+      theme: TodoListUiConfig.theme,
       routes: {
-        '/login': (_) => MultiProvider(
-              providers: [
-                ChangeNotifierProvider(create: (_) => LoginController()),
-              ],
-              child: LoginPage(),
-            )
+        ...AuthModule().routers,
       },
       home: SplashPage(),
     );
--- lib/app/core/modules/todo_list_module.dart
+++ lib/app/core/modules/todo_list_module.dart
@@ -1,5 +1,8 @@
+//Base de todas as classes
+
 import 'package:flutter/material.dart';
 import 'package:provider/single_child_widget.dart';
+import 'package:todo_list_provider/app/core/modules/todo_list_page.dart';
 
 abstract class TodoListModule {
   final Map<String, WidgetBuilder> _routers;
@@ -10,4 +13,9 @@ abstract class TodoListModule {
       required Map<String, WidgetBuilder> routers})
       : _routers = routers,
         _bindings = bindings;
+
+  Map<String, WidgetBuilder> get routers {
+    return _routers.map((key, pageBuilder) => MapEntry(
+        key, (_) => TodoListPage(bindings: _bindings, page: pageBuilder)));
+  }
 }
